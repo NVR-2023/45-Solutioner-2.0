abc85bf27ad1774d9529b9b87ab2592b5012bed7
NVR <nunoviolanterodrigues@hotmail.com>
2024-01-22 18:09:53 +0000

    Set Navbar component layout

--- src/sections/navbar/navbar.tsx
+++ src/sections/navbar/navbar.tsx
@@ -2,14 +2,12 @@ import React, { FC } from "react";
 
 const Navbar: FC = () => {
   return (
-    <header>
-      <nav className="relative z-[999]">
-        <div className="">
-          <div className="">
+    <header className="relative z-50 w-full">
+      <nav className="fixed top-0 w-full">
+        <div className="flex justify-between rounded mx-5 px-3 py-1 bg-red-300 ">
             <div>Logo</div>
             <div>Primary links</div>
             <div>Secondary links</div>
-          </div>
         </div>
       </nav>
     </header>
