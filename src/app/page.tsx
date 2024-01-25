ba093f80f94c2e005182499816f7a72cf6441a27
NVR <nunoviolanterodrigues@hotmail.com>
2024-01-25 00:27:31 +0000

    Customized scroll bar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,17 +3,18 @@ import ServicesHomeSection from "@/sections/servicessectiomhome/servicessectionh
 import SecuritySectionHome from "@/sections/securitysectionhome/securitysectionhome";
 import PricingSectionHome from "@/sections/pricingsectionhome/pricingsectionhome";
 
+
 export default function Home() {
   return (
-    <main className="">
+    <main id="homePage" className="">
       <HomeNavbar />
-      <div className="border-b-4 border-neutral-300">
+      <div className="border-b-2 border-neutral-300">
         <ServicesHomeSection />
       </div>
-      <div className="border-b-4 border-neutral-300">
+      <div className="border-b-2 border-neutral-300">
         <SecuritySectionHome />
       </div>
-      <div className="border-b-4 border-neutral-300">
+      <div className="border-b-2 border-neutral-300">
         <PricingSectionHome />
       </div>
     </main>
