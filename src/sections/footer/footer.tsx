ee8fea8d5f4299eed950e5854f7022346faaa48c
NVR <nunoviolanterodrigues@hotmail.com>
2024-01-25 20:57:50 +0000

    Changes to footer

--- src/sections/footer/footer.tsx
+++ src/sections/footer/footer.tsx
@@ -7,7 +7,9 @@ import SocialMediaIcons from "./social-media-icons-footer";
 const Footer: FC = () => {
   type FooterLinkColumnType = {
     label: string;
-    links: string[];
+    links?: string[];
+    socialMediaIcons?: { children: ReactNode };
+    caption?: string;
   };
 
   type FooterLinkListType = FooterLinkColumnType[];
@@ -18,46 +20,47 @@ const Footer: FC = () => {
       links: ["About us", "Contact us", "Providers", "Jobs", "Press kit"],
     },
     { label: "legal", links: ["Terms of use", "Privacy policy", "Cookie policy"] },
+    {
+      label: "social media",
+      socialMediaIcons: { children: <SocialMediaIcons /> },
+      caption: `© ${getCurrentYear()} Nuno Rodrigues`,
+    },
   ];
 
   return (
-    <footer id="footer" className="py-10 md:py-5 bg-zinc-900">
+    <footer id="footer" className="py-5 bg-zinc-900">
       <div className="flex justify-center ">
-      <nav className="flex space-x-3 md:space-x-48 ">
-        {footerLinkArray.map((linksColumn, index) => (
-          <div className="block w-4/12 space-y-0.5" key={index}>
-            <div
-              className=" font-aperçu font-bold text-md tracking-wider text-neutral-500 "
-              style={{ fontVariant: "small-caps" }}
-              key={`column-${index}`}>
-              {linksColumn.label}
-            </div>
-            {linksColumn.links.map((link, index) => (
-              <div className="" key={index}>
-                <Link
-                  className=" font-aperçu font-bold text-xs tracking-wide text-neutral-300"
-                  href={getURLfromString(link)}>
-                  {link}
-                </Link>
+        <nav className="flex space-x-3 md:space-x-32 ">
+          {footerLinkArray.map((linksColumn, index) => (
+            <div className="block md:w-48" key={index}>
+              <div
+                className=" font-aperçu font-bold text-md tracking-wider text-neutral-500 mb-2 "
+                style={{ fontVariant: "small-caps" }}
+                key={`column-${index}`}>
+                {linksColumn.label}
               </div>
-            ))}
-          </div>
-        ))}
-
-        <div className="block w-4/12 space-y-0.5 ">
-          <div
-            className=" font-aperçu font-bold text-md tracking-wider text-neutral-500 "
-            style={{ fontVariant: "small-caps" }}>
-            social media
-          </div>
-          <div className="">
-            <SocialMediaIcons />
-          </div>
-          <div className=" font-aperçu font-bold text-xs tracking-wide text-neutral-300">
-            {`© ${getCurrentYear()} Nuno Rodrigues`}
-          </div>
-        </div>
-      </nav>
+              <ul className="">
+                {linksColumn.links?.map((link, index) => (
+                  <li
+                    className=" font-aperçu font-bold text-xs tracking-wide text-neutral-300 mb-1.5"
+                    key={index}>
+                    <Link href={getURLfromString(link)}>{link}</Link>
+                  </li>
+                ))}
+                {linksColumn.socialMediaIcons && (
+                  <li key="social-media-icons">{linksColumn.socialMediaIcons.children}</li>
+                )}
+                {linksColumn.caption && (
+                  <li
+                    className=" font-aperçu font-bold text-xs tracking-wide text-neutral-300"
+                    key="caption">
+                    {linksColumn.caption}
+                  </li>
+                )}
+              </ul>
+            </div>
+          ))}
+        </nav>
       </div>
     </footer>
   );
