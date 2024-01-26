d1949533287432ddfefab31c14d31a713c1592bb
NVR <nunoviolanterodrigues@hotmail.com>
2024-01-26 00:08:09 +0000

    Changes to sidemenu / homepage

--- src/sections/navbar-homepage/mobile-sidemenu-homepage.tsx
+++ src/sections/navbar-homepage/mobile-sidemenu-homepage.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import React, { FC } from "react";
 import sectionLinksArray from "./section-list-array.t";
 import Link from "next/link";
@@ -6,30 +8,31 @@ import RegisterButton from "./register-button";
 import { getSectionFomString } from "@/utils/getsectionfromurl";
 
 const MobileSidemenuHomepage: FC = () => {
-  return <div className="z-[999] absolute right-0 h-full w-40 bg-red-400"></div>;
-  {
-    <nav className="absolute left-0 -translate-y-5 px-2 bg-neutral-200 ">
-      <ul className="mt-5 ">
-        {sectionLinksArray &&
-          sectionLinksArray.map((section, index) => (
-            <li
-              key={index}
-              className="ms-1 mb-1.5 font-aperçu font-bold text-base text-green-700 tracking-wide"
-              onClick={() => {
-                console.log("sss");
-              }}>
-              <Link href={getSectionFomString(section)}>{section}</Link>
-            </li>
-          ))}
-        <li className="" key="signInButton">
-          <SigninButton />
-        </li>
-        <li className="" key="registerButton">
-          <RegisterButton />
-        </li>
-      </ul>
-    </nav>;
-  }
+  return (
+    <div className="z-[999] absolute right-0 h-full w-1/4 bg-neutral-200">
+      <nav className="flex justify-center">
+        <ul className="mt-5">
+          {sectionLinksArray &&
+            sectionLinksArray.map((section, index) => (
+              <li
+                key={index}
+                className="ms-1 mb-1.5 font-aperçu font-bold text-base text-green-700 tracking-wide"
+                onClick={() => {
+                  console.log("sss");
+                }}>
+                <Link href={getSectionFomString(section)}>{section}</Link>
+              </li>
+            ))}
+          <li className="mt-16" key="signInButton">
+            <SigninButton />
+          </li>
+          <li className="mt-4" key="registerButton">
+            <RegisterButton />
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
 };
 
 export default MobileSidemenuHomepage;
