17237ebf4023014891a1d328d2f6c3adce97cdbf
NVR <nunoviolanterodrigues@hotmail.com>
2024-02-02 23:51:40 +0000

    Change to Callback

--- src/sections/navbar-homepage/navbar-homepage.tsx
+++ src/sections/navbar-homepage/navbar-homepage.tsx
@@ -12,9 +12,9 @@ const NavbarHomepage: FC = () => {
   const { isDarkThemeOn } = useThemeContext();
 
   return (
-    <header className={`relative z-50 w-full flex justify-center  ${isDarkThemeOn ? "dark" : "" }`}>
-      <nav className={`fixed justify-center top-0 md:top-3 mx-7 dark:bg-red-500 rounded`}>
-        <div className="flex align-middle justify-between mx-4 md:mx-12 px-3 py-1 dark:bg-green-500 ">
+    <header className={`relative z-50 w-full flex bg-blue-500 justify-center  ${isDarkThemeOn ? "dark" : "" }`}>
+      <nav className={`fixed justify-center top-0 md:top-3 dark:bg-red-500 rounded`}>
+        <div className="flex align-middle justify-between mx-3 py-3 dark:bg-green-500 ">
           <div className="">
             <LogoSegment />
           </div>
