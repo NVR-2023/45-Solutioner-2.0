9ce361900b9fdab3d3e8a394cf5502f321676269
NVR <nunoviolanterodrigues@hotmail.com>
2024-02-02 20:10:27 +0000

    Fix themes for mobile menu

--- src/app/global-text-styles.tsx
+++ src/app/global-text-styles.tsx
@@ -4,25 +4,26 @@ export const DARK_THEME_BACKGROUND: string = " dark:bg-neutral-700";
 export const LIGHT_THEME_BASE_TEXT: string = " font-aperçu text-black";
 export const DARK_THEME_BASE_TEXT: string = " dark:font-aperçu dark:text-white";
 
-export const LIGHT_THEME_DEFAULT: string = LIGHT_THEME_BACKGROUND + LIGHT_THEME_BASE_TEXT
+export const LIGHT_THEME_DEFAULT: string = LIGHT_THEME_BACKGROUND + LIGHT_THEME_BASE_TEXT;
 export const DARK_THEME_DEFAULT: string = DARK_THEME_BACKGROUND + DARK_THEME_BASE_TEXT;
 
-
-
 export const LOGO_TEXT: string = LIGHT_THEME_BASE_TEXT + " font-bold text-lg tracking-wide";
-export const HOME_NAVBAR_LINKS_TEXT: string = LIGHT_THEME_BASE_TEXT + " font-semibold text-sm";
+export const HOME_NAVBAR_LINKS_TEXT: string = " font-semibold text-sm";
 
-export const LIGHT_THEME_FOOTER_LABELS: string = LIGHT_THEME_BASE_TEXT + " text-sm font-black font-small-caps tracking-wider";
-export const DARK_THEME_FOOTER_LABELS: string = DARK_THEME_BASE_TEXT +
-  " dark:text-sm dark:font-black dark:font-small-caps dark:tracking-wider";
+export const LIGHT_THEME_FOOTER_LABELS: string =
+  LIGHT_THEME_BASE_TEXT + " text-sm font-black font-small-caps tracking-wider";
+export const DARK_THEME_FOOTER_LABELS: string =
+  DARK_THEME_BASE_TEXT + " dark:text-sm dark:font-black dark:font-small-caps dark:tracking-wider";
 
-export const LIGHT_THEME_FOOTER_TEXT: string = LIGHT_THEME_BASE_TEXT + " text-xs font-semibold text-sm";
-export const DARK_THEME_FOOTER_TEXT: string = DARK_THEME_BASE_TEXT + " dark:text-xs dark:font-semibold dark:text-sm";
+export const LIGHT_THEME_FOOTER_TEXT: string =
+  LIGHT_THEME_BASE_TEXT + " text-xs font-semibold text-sm";
+export const DARK_THEME_FOOTER_TEXT: string =
+  DARK_THEME_BASE_TEXT + " dark:text-xs dark:font-semibold dark:text-sm";
 
 export const LIGHT_THEME_FOOTER_CAPTION: string =
   LIGHT_THEME_BASE_TEXT + " font-semibold text-[0.625rem] tracking-wide";
-export const DARK_THEME_FOOTER_CAPTION: string = DARK_THEME_BASE_TEXT + " dark:font-semibold dark:tracking-wide";
-
+export const DARK_THEME_FOOTER_CAPTION: string =
+  DARK_THEME_BASE_TEXT + " dark:font-semibold dark:tracking-wide";
 
 export const LIGHT_THEME_OUTLINED_BUTTON_TEXT: string = "";
 export const DARK_THEME_OUTLINED_BUTTON_TEXT: string = "dark:font-bold";
--- src/sections/navbar-homepage/sub-components/mobile-sidemenu-homepage.tsx
+++ src/sections/navbar-homepage/sub-components/mobile-sidemenu-homepage.tsx
@@ -2,11 +2,12 @@ import React, { FC } from "react";
 import homepageSections from "@/sections/navbar-homepage/sub-components/homepageSections";
 import Link from "next/link";
 import { getSectionFomString } from "@/utils/functions/getsectionfromurl";
+import { useThemeContext } from "@/contexts/theme-context";
 
 import {
   HOME_NAVBAR_LINKS_TEXT,
-  LIGHT_THEME_BACKGROUND,
-  DARK_THEME_BACKGROUND,
+  LIGHT_THEME_DEFAULT,
+  DARK_THEME_DEFAULT,
 } from "@/app/global-text-styles";
 
 type MobileSidemenuPropsType = {
@@ -14,16 +15,17 @@ type MobileSidemenuPropsType = {
 };
 
 const MobileSidemenuHomepage: FC<MobileSidemenuPropsType> = ({ action }) => {
+  const { isDarkThemeOn  } = useThemeContext();
   return (
-    <nav>
-      <div className={`z-[999] top-3 right-3 ${LIGHT_THEME_BACKGROUND}`}>
+    <nav className={isDarkThemeOn ? "dark" : ""}>
+      <div className={`z-[999] top-3 right-3 ${LIGHT_THEME_DEFAULT} ${DARK_THEME_DEFAULT}`}>
         <div className="flex justify-start">
           <ul className="list-none flex flex-col space-y-1 mt-3 mx-3">
             {homepageSections &&
               homepageSections.map((section, index) => (
                 <li key={index} onClick={action}>
                   <Link
-                    className={`${HOME_NAVBAR_LINKS_TEXT} pb-[2px] border-b-[1px] border-solid border-transparent transition-all duration-300 hover:border-black`}
+                    className={`${HOME_NAVBAR_LINKS_TEXT} pb-[2px] border-b-[1px] border-solid border-transparent transition-all duration-300 hover:border-current`}
                     href={getSectionFomString(section)}>
                     {section}
                   </Link>
