f1c780f13e45219ad09655e489c7eec8ea3f62d2
NVR <nunoviolanterodrigues@hotmail.com>
2024-02-02 00:55:32 +0000

    Add ! to smooth-scroll

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="scroll-smooth">
+    <html lang="en" className="!scroll-smooth">
       <body className={`${aperçuFont.variable}`}>
         <ThemeContextProvider>{children}</ThemeContextProvider>
       </body>
