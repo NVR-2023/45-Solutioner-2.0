6a19abc9b4e46b09b15364dd895d0277ded0d785
NVR <nunoviolanterodrigues@hotmail.com>
2024-02-17 13:51:36 +0000

    fix: fix social media icons

--- src/sections/footer/sub-components/social-media-icons-footer.tsx
+++ src/sections/footer/sub-components/social-media-icons-footer.tsx
@@ -6,9 +6,30 @@ import LinkedinIcon from "../../../components/icons/linkedin-icon";
 const SocialMediaIcons: FC = () => {
   return (
     <nav className="flex space-x-5 h-42">
-      <XIcon scale={0.7} />
-      <FacebookIcon scale={0.7} />
-      <LinkedinIcon scale={0.7} />
+      <a
+        aria-label="X"
+        href="https://www.twitter.com"
+        title="twitter.com"
+        target="_blank"
+        rel="noopener noreferrer">
+        <XIcon scale={0.7} />
+      </a>
+      <a
+        aria-label="Facebook"
+        href="https://www.facebook.com"
+        title="facebook.com"
+        target="_blank"
+        rel="noopener noreferrer">
+        <FacebookIcon scale={0.7} />
+      </a>
+      <a
+        aria-label="Linkedin"
+        href="https://www.linkedin.com"
+        title="linkedin.com"
+        target="_blank"
+        rel="noopener noreferrer">
+        <LinkedinIcon scale={0.7} />
+      </a>
     </nav>
   );
 };
