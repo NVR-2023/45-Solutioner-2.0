f04f4d0f9ebdc48af6424fb930fb768d0e0a3d04
NVR <nunoviolanterodrigues@hotmail.com>
2024-02-21 18:39:29 +0000

    fix: remove context from services/page.tsx

--- src/app/private/services/page.tsx
+++ src/app/private/services/page.tsx
@@ -1,10 +1,8 @@
 "use client"
 
 import React , {FC} from "react"
-import { useThemeContext } from "@/contexts/theme-context";
 
 const Services:FC = () => {
-    const { isDarkThemeOn , setIsDarkThemeOn } = useThemeContext();
   return (
     <div>
         <p>Hello Services</p>
