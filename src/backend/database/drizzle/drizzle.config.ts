e89227db875cd7fc682857ed5e32d3a27cc5877e
NVR <nunoviolanterodrigues@hotmail.com>
2024-02-27 15:19:56 +0000

    chore: commit before switching branches

---
+++ src/backend/database/drizzle/drizzle.config.ts
@@ -0,0 +1,13 @@
+import "./config.ts";
+import { defineConfig } from "drizzle-kit";
+
+export default defineConfig({
+  schema: "/src/backend/database/schema/schema.ts",
+  out: "/src/backend/database/drizzle/migrations",
+  driver: "pg",
+  dbCredentials: {
+    connectionString: process.env.POSTGRES_URL! + "?sslmode=require",
+  },
+  verbose: true,
+  strict: true,
+});
