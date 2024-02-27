c846abd1aebd3ea07981a878540339e2d4ae1d39
NVR <nunoviolanterodrigues@hotmail.com>
2024-02-27 22:08:14 +0000

    chore: test reading from the database

--- src/app/private/services/page.tsx
+++ src/app/private/services/page.tsx
@@ -1,36 +1,17 @@
-"use client"
+import { getUsers , getUsers2 } from "@/backend/database/drizzle/db";
 
 import React , {FC} from "react"
 
-const Services:FC = () => {
+const Services:FC = async () => {
+   const data = await getUsers();
+   const data2 = await getUsers2();
+
   return (
     <div>
-        <p>Hello Services</p>
-        <p>Hello Services</p>
-        <p>Hello Services</p>
-        <p>Hello Services</p>
-        <p>Hello Services</p>
-        <p>Hello Services</p>
-        <p>Hello Services</p>
-        <p>Hello Services</p>
-        <p>Hello Services</p>
-        <p>Hello Services</p>
-        <p>Hello Services</p>
-        <p>Hello Services</p>
-        <p>Hello Services</p>
-        <p>Hello Services</p>
-        <p>Hello Services</p>
-        <p>Hello Services</p>
-        <p>Hello Services</p>
-        <p>Hello Services</p>
-        <p>Hello Services</p>
-        <p>Hello Services</p>
-        <p>Hello Services</p>
-        <p>Hello Services</p>
-        <p>Hello Services</p>
-      
+      <div>sql-like: {JSON.stringify(data)}</div>
+      <div>relational: {JSON.stringify(data2)}</div>
     </div>
-  )
+  );
 };
 
 export default Services;
