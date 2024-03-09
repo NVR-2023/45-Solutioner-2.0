7e781dbc866dfb123a4a22b57d09fc87dc95bb3e
NVR <nunoviolanterodrigues@hotmail.com>
2024-03-09 00:04:23 +0000

    chore: structure register modal

--- src/frontend/sections/register-modal/register-modal.tsx
+++ src/frontend/sections/register-modal/register-modal.tsx
@@ -3,7 +3,12 @@ import ModalShell from "@/frontend/components/ui/modal-shell/modal-shell";
 const RegisterModal = () => {
   return (
     <ModalShell>
-      <div className="h-full ">dfsdfdsfsdf</div>
+      <div className="flex flex-col">
+        <div className="h-16 p-4 border-[D9D9D9] border-b-[0.5px]">
+            
+        </div>
+        <div className="p-4 "></div>
+      </div>
     </ModalShell>
   );
 };
