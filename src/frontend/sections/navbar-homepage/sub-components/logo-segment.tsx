2221b4c1a870200ad0ecde4bf2c63c6e6dedf4dc
Nuno Rodrigues <123973723+NVR-2023@users.noreply.github.com>
2024-04-10 13:33:23 +0100

    feat: add logo

--- src/frontend/sections/navbar-homepage/sub-components/logo-segment.tsx
+++ src/frontend/sections/navbar-homepage/sub-components/logo-segment.tsx
@@ -1,9 +1,10 @@
 import Link from "next/link";
-
+import LogoIcon from "@/frontend/components/icons/logo-icon";
 const LogoSegment = () => {
   return (
     <div
-      className="h-full flex items-center font-rozhaOne text-[1.25rem] tracking-wider">
+      className="h-full flex items-center font-semibold text-[1rem] text-[#ff7714] space-x-1">
+      <LogoIcon />
       <Link href="/#services">Solutioner</Link>
     </div>
   );
