c6eae4bb181b4048a3b5ea0b793e00bc52f78099
Nuno Rodrigues <123973723+NVR-2023@users.noreply.github.com>
2024-04-15 23:05:19 +0100

    chore:

--- src/frontend/sections/navbar-homepage/sub-components/logo-segment.tsx
+++ src/frontend/sections/navbar-homepage/sub-components/logo-segment.tsx
@@ -3,7 +3,7 @@ import LogoIcon from "@/frontend/components/icons/logo-icon";
 const LogoSegment = () => {
   return (
     <div
-      className="h-full flex items-center font-semibold text-[1rem] text-[#ff7714] space-x-[1px]">
+      className="h-full flex items-center font-semibold text-[1rem] text-[#ff7714] space-x-[1px] ">
       <span className="transform translate-y-[1pt]"><LogoIcon /></span>
       <span><Link href="/#services">Solutioner</Link></span>
     </div>
