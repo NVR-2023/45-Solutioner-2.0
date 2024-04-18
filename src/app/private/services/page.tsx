f231ff50f03b795fc3fbb19f6e437b4d7d32ab5b
Nuno Rodrigues <123973723+NVR-2023@users.noreply.github.com>
2024-04-18 23:15:32 +0100

    chore: improve services tab layout

--- src/app/private/services/page.tsx
+++ src/app/private/services/page.tsx
@@ -14,13 +14,13 @@ const Services = async () => {
 
 
   return (
-    <div className="m-0 h-screen w-screen bg-neutral-100 py-2 pe-8 ps-6 dark:bg-green-400">
-      <div className="grid h-full w-full grid-cols-12 grid-rows-12 space-y-2">
-        <header className="col-span-12 row-span-2 flex flex-col space-y-2 bg-neutral-200">
-          <div className="flex-grow"><PrivateNavbar /></div>
-          <div className="flex-grow"><ServicesContentNavbar /></div>
+    <div className="m-0 h-screen w-screen bg-neutral-200 py-2 pe-8 ps-6 dark:bg-gree">
+      <div className="h-full w-full flex flex-col space-y-2">
+        <header className="flex flex-col space-y-2 bg-neutral-200">
+          <div className="h-10"><PrivateNavbar /></div>
+          <div className="h-10"><ServicesContentNavbar /></div>
         </header>
-        <main className="col-span-12 row-span-10 overflow-y-auto bg-neutral-100">
+        <main className=" flex-grow overflow-y-auto bg-neutral-100">
           <p>123</p>
           <p>123</p>
           <p>123</p>
--- src/app/private/services/sub-components/services-content-navbar.tsx
+++ src/app/private/services/sub-components/services-content-navbar.tsx
@@ -1,9 +1,9 @@
 const ServicesContentNavbar = () => {
   return (
-    <div className="h-full w-full m-0 p-0 gap-0 rounded bg-neutral-300">
+    <div className="m-0 flex h-full w-full items-center gap-0 rounded font-medium bg-neutral-300 p-0 px-8 py-2 font-aperçu text-sm">
       Content Navbar
     </div>
-  )
+  );
 };
 
 export default ServicesContentNavbar;
