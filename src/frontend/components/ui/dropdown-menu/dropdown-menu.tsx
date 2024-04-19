3a6ef566f4279dfba877211f3f8fe5a4f3a6ea22
Nuno Rodrigues <123973723+NVR-2023@users.noreply.github.com>
2024-04-19 21:45:02 +0100

    feat: improve dropdown menu

--- src/frontend/components/ui/dropdown-menu/dropdown-menu.tsx
+++ src/frontend/components/ui/dropdown-menu/dropdown-menu.tsx
@@ -15,30 +15,53 @@ const DropdownMenu = ({ menuLabel, menuEntries }: DropDownMenuProps) => {
     setIsMenuOpen(!isMenuOpen);
   };
 
+  const handleOnMouseEnter = () => {
+    setIsMenuOpen(true);
+  };
+  const handleOnMouseLeave = () => {
+    setIsMenuOpen(false);
+  };
+
+  const handleOnMenuMouseEnter = () => {
+    setIsMenuOpen(true);
+  }
+
+  const handleOnMenuMouseLeave = () => {
+    setIsMenuOpen(false);
+  }
+
+  
+
   const handleOnSelect = () => {};
 
   return (
-    <div className="relative block px-2">
+    <div className="relative flex items-center px-2">
       <button
         onClick={handleOnToggle}
-        className="flex w-full items-center space-x-2"
+        onMouseEnter={handleOnMouseEnter}
+        onMouseLeave={handleOnMouseLeave}
+        className="flex w-full h-14 items-center space-x-2"
       >
         <span className="flex">
-          <InputFieldLabel label={menuLabel + ":"} />
-          {`: ${menuEntries[0]}`}
+          <InputFieldLabel label={menuLabel + ":\u00A0"} />
+          {`${menuEntries[0]}`}
         </span>
         <span
-          className={`origin-center flex items-center transition-all duration-300 ${isMenuOpen ? "rotate-180" : ""} `}
+          className={`flex origin-center items-center transition-all duration-300 ${isMenuOpen ? "rotate-180" : ""} `}
         >
-          <MenuDownArrow scale={0.5} />
+          <MenuDownArrow scale={0.6125} />
         </span>
       </button>
       {isMenuOpen && (
-        <menu className=" absolute left-0 top-8 block w-full rounded-[2px] bg-green-300 px-2 py-2">
+        <menu
+          onMouseEnter={handleOnMenuMouseEnter}
+          onMouseLeave={handleOnMenuMouseLeave}
+          className="px-2 absolute left-0 top-14 block w-full rounded-[2px] bg-green-300 py-2"
+        >
           {menuEntries.map((entry, index) => {
             return (
               <option
-                className=" hover:w-50 rounded-[2px] px-2 hover:bg-purple-400 active:bg-yellow-500"
+                className="m-0 p-0 flex justify-start  hover:w-50 rounded-[2px]  hover:bg-purple-400 active:bg-yellow-500"
                 key={index}
               >
                 {entry}
