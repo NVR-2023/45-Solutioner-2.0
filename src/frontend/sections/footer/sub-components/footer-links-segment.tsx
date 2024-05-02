d730a88143beb43d2d86509835918e75cd593df4
Nuno Rodrigues <123973723+NVR-2023@users.noreply.github.com>
2024-05-02 17:46:39 +0100

    chore:

--- src/frontend/sections/footer/sub-components/footer-links-segment.tsx
+++ src/frontend/sections/footer/sub-components/footer-links-segment.tsx
@@ -12,21 +12,24 @@ type LinkListProps = {
 const FooterLinksSegment = ({linkList}: LinkListProps) => {
   return (
     <nav key={linkList.label}>
-      <div className="flex flex-col w-26 md:w-64">
+      <div className="w-26 flex flex-col md:w-64">
         <label
-          className="text-sm font-bold small-caps tracking-wide md:mb-2 pe-2 md:pe-0"
-          id="footerHeading">
+          className="pe-2 text-sm font-bold tracking-wide small-caps md:mb-2 md:pe-0"
+          id="footerHeading"
+        >
           {linkList.label}
         </label>
         <menu
-          className="list-none flex flex-row md:flex-col space-x-0.5 md:space-x-0 md:space-y-2"
-          aria-label="List of Links">
+          className="flex list-none flex-row space-x-0.5 md:flex-col md:space-x-0 md:space-y-2"
+          aria-label="List of Links"
+        >
           {linkList.links?.map((link, index) => (
             <li className="flex" key={index}>
               <Link
-                className="text-sm md:text-xs font-semibold pb-[1px] px-1 md:px-0 border-b-[1px] border-solid border-transparent transition-all duration-300 hover:border-current"
-                href={getURLfromString(link)}
-                aria-current="page">
+                className="border-b-[1px] border-solid border-transparent px-1 pb-[1px] text-sm font-semibold transition-all duration-300 hover:border-current md:px-0 md:text-xs"
+                href={`/footer/${getURLfromString(link)}`}
+                aria-current="page"
+              >
                 {link}
               </Link>
             </li>
