f1bc0ef939b4556c49bd2c8c81a7c493e06c76da
Nuno Rodrigues <123973723+NVR-2023@users.noreply.github.com>
2024-05-05 16:13:48 +0100

    chore:

--- src/app/private/main/book/sub-components/content-area-bool-page.tsx
+++ src/app/private/main/book/sub-components/content-area-bool-page.tsx
@@ -2,6 +2,7 @@ import { motion, stagger } from "framer-motion";
 import { AllServiceStaticDataType } from "@/utils/functions/fetch-data/services-endpoint-submissions";
 
 import ServiceCard from "./service-card";
+import { useSearchParams } from "next/navigation";
 
 type ContentAreaBookPageProps = {
   allServicesStaticData: AllServiceStaticDataType | null;
@@ -10,24 +11,46 @@ type ContentAreaBookPageProps = {
 const ContentAreaBookPage = ({
   allServicesStaticData,
 }: ContentAreaBookPageProps) => {
+  const searchParams = useSearchParams();
+  const categorySearchParam = searchParams.get("category");
+  const priceSearchParam = searchParams.get("price");
+  const sortBySearchParam = searchParams.get("sort by");
+  const searchSearchParam = searchParams.get("search");
+
   return (
-    <div>
+    <motion.div className="flex justify-center py-2">
       {allServicesStaticData && (
         <motion.ul
           key="filtered and sorted list of services"
           className="space-y-2"
         >
-          {allServicesStaticData.map((service) => (
-            <li key={service.id} className="">
-              <ServiceCard service={service.service} id={service.id} />
-            </li>
+          <div >category:...{categorySearchParam}</div>
+          <div>price:...{priceSearchParam}</div>
+          <div>sort by:...{sortBySearchParam}</div>
+          <div>search:...{searchSearchParam}</div>
+
+          {allServicesStaticData.map((service, index) => (
+            <motion.li
+              initial={{ opacity: 0 }}
+              animate={{
+                opacity: 1,
+                transition: { duration: 0.1, delay: 0.05 * index },
+              }}
+              key={service.id}
+            >
+              <ServiceCard
+                service={service.service}
+                price={service.price}
+                included={service.included}
+                description={service.description}
+                id={service.id}
+              />
+            </motion.li>
           ))}
         </motion.ul>
       )}
-    </div>
+    </motion.div>
   );
 };
-
-
 
 export default ContentAreaBookPage;
--- src/app/private/main/book/sub-components/service-card.tsx
+++ src/app/private/main/book/sub-components/service-card.tsx
@@ -1,14 +1,29 @@
+import { motion } from "framer-motion";
 type ServiceCardProps = {
   service: string;
   id: number;
+  price: string;
+  included: string;
+  description: string;
 };
 
-const ServiceCard = ({ service, id }: ServiceCardProps) => {
+const variants = {
+  animate: {
+    transition: {
+      staggerChildren: 0.4,
+    },
+  },
+};
+
+const ServiceCard = ({ service, id, price, description, included }: ServiceCardProps) => {
   return (
-    <div className="flex space-x-2 rounded h-10 w-80 items-center justify-center bg-neutral-300">
-     <span>{service}</span> 
-     <span>{id}</span> 
-    </div>
+    <motion.div 
+    variants={variants}
+    className="flex space-x-2 rounded h-6 w-[50rem] items-center justify-center bg-neutral-300">
+     <motion.span>{service}</motion.span> 
+     <motion.span>{id}</motion.span> 
+     <motion.span>{price}</motion.span> 
+    </motion.div>
   );
 };
 
