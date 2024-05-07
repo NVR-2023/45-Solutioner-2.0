9ef5172974f45ed66be7ce344975e3f0bf49a3ff
Nuno Rodrigues <123973723+NVR-2023@users.noreply.github.com>
2024-05-07 21:26:24 +0100

    feat: add expandable area to service rollup

--- src/app/private/main/book/sub-components/service-rollup.tsx
+++ src/app/private/main/book/sub-components/service-rollup.tsx
@@ -1,7 +1,38 @@
+import { useState } from "react";
+
 const ServiceRollup = () => {
+  const [isServiceRollupHovered, setIsServiceRollupHovered] = useState(false);
+
+  const handleOnEnter = () => {
+    setIsServiceRollupHovered(true);
+  };
+
+  const handleOnLeave = () => {
+    setIsServiceRollupHovered(false);
+  };
+
   return (
-    <div className="w-[50rem] h-7 rounded bg-red-400">
-      <div className="h-full w-full">123</div>
+    <div className="">
+      <div
+        onMouseEnter={handleOnEnter}
+        onMouseLeave={handleOnLeave}
+        className={` h-7 w-[700px] bg-red-400 ${isServiceRollupHovered ? "rounded-t-lg" : "rounded-lg"}`}
+      >
+        <div className="h-full w-full">123</div>
+      </div>
+
+      <div
+        className="grid"
+        style={{
+          gridTemplateRows: isServiceRollupHovered ? "1fr" : "0fr",
+          transition: "grid-template-rows 500ms",
+        }}
+      >
+        <div className="overflow-hidden rounded-b bg-green-400 ">
+          <div className="overflow-hidden">skfljlsdhflshfljksdfhsdljf</div>
+        </div>
+      </div>
+      <div></div>
     </div>
   );
 };
