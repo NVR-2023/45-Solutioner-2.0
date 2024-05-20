ccea076a0f29adfc3c3865c6c01f0f1a7e6b5e79
Nuno Rodrigues <123973723+NVR-2023@users.noreply.github.com>
2024-05-20 21:47:59 +0100

    chore:

--- src/frontend/sections/service-rollup/sub-components/book-service-button.tsx
+++ src/frontend/sections/service-rollup/sub-components/book-service-button.tsx
@@ -1,29 +1,28 @@
 import { useBookServiceModalContext } from "@/frontend/contextes/use-book-service-modal-context";
 import { useEffect } from "react";
 
-
 import { BookServiceModalObjectType } from "@/frontend/contextes/use-book-service-modal-context";
 
 const BookServiceButton = () => {
   const { bookServiceModalObject, setBookServiceModalObject } =
     useBookServiceModalContext();
-  
+
   const xl = bookServiceModalObject?.service;
-  
-   useEffect(() => {
-     setBookServiceModalObject((current: BookServiceModalObjectType) => ({
-       ...current,
-       service: "abracadabra",
-     }));
-   }, []);
-   
+
+  useEffect(() => {
+    setBookServiceModalObject((current: BookServiceModalObjectType) => ({
+      ...current,
+      service: "book",
+    }));
+  }, []);
+
   const handleOnClick = () => {};
 
   return (
     <div className="flex h-full items-center">
       <button
         onClick={handleOnClick}
-        className="h-4 rounded-[2px] bg-neutral-900 px-3 font-aperçu text-sm font-bold leading-[.5rem] tracking-wide text-neutral-300 small-caps dark:font-semibold dark:text-neutral-300 md:text-xs"
+        className="h-4 rounded-[2px] bg-neutral-500 px-3 font-aperçu text-sm font-bold leading-[.5rem] tracking-wide text-neutral-300 small-caps dark:font-semibold dark:text-neutral-300 md:text-xs"
       >
         {xl}
       </button>
