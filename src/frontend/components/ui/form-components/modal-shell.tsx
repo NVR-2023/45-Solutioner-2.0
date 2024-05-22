bc2d5184591f16099e34fced7b474d3c58963599
Nuno Rodrigues <123973723+NVR-2023@users.noreply.github.com>
2024-05-22 22:51:03 +0100

    chore:

--- src/frontend/components/ui/form-components/modal-shell.tsx
+++ src/frontend/components/ui/form-components/modal-shell.tsx
@@ -38,7 +38,7 @@ const ModalShell = ({ isModalOpen, children }: FormModalShellProps) => {
             animate="animate"
             exit="exit"
             variants={variants}
-            className="z-50 flex w-[18rem] justify-center rounded bg-neutral-300 px-14 py-7 text-black shadow-[18px_18px_12px_0px_#00000040] dark:text-neutral-300"
+            className="z-[9999] flex w-[18rem] justify-center rounded bg-neutral-300 px-14 py-7 text-black shadow-[18px_18px_12px_0px_#00000040] dark:text-neutral-300"
           >
             {children}
           </motion.div>
