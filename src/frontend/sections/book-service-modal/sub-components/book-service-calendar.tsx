66dd5ba95f3b4947cd16c6d1da16437dab9df27a
Nuno Rodrigues <123973723+NVR-2023@users.noreply.github.com>
2024-06-01 22:39:35 +0100

    chore: change rollup border color

--- src/frontend/sections/book-service-modal/sub-components/book-service-calendar.tsx
+++ src/frontend/sections/book-service-modal/sub-components/book-service-calendar.tsx
@@ -45,7 +45,7 @@ const BookServiceCalendar = ({
   return (
     <div className="flex w-full flex-col justify-center space-y-0.5 rounded bg-neutral-300 px-2 pb-2">
       <header className="mt-2">
-        <div className="grid grid-cols-7 grid-rows-1 border-b-[.7px]  border-black">
+        <div className="grid grid-cols-7 grid-rows-1">
           {DAYS_OF_THE_WEEK_ABBREVIATIONS.map((weekDayAbbreviation, index) => (
             <div key={index} className="flex items-end ">
               <span className="flex w-full items-center justify-center py-0.5 font-aperçu text-xs font-bold small-caps dark:text-neutral-300">
@@ -56,77 +56,76 @@ const BookServiceCalendar = ({
         </div>
       </header>
 
-      <main className="space-y-4">
-      <div className="grid grid-cols-7 grid-rows-5 space-y-0.5">
-        {Array.from({ length: 5 }).map((week, weekIndex) => (
-          <div
-            key={weekIndex}
-            className="col-span-7 row-span-1 grid grid-cols-7"
-          >
-            {Array.from({ length: 7 }).map((day, dayIndex) => {
-              const movingDate = new Date(mostRecentSunday);
-              movingDate.setDate(
-                mostRecentSunday.getDate() + (weekIndex * 7 + dayIndex),
-              );
-              movingDate.setHours(0, 0, 0, 0);
-              const dayOfTheMonth = movingDate.getDate();
+      <main className="">
+        <div className="grid grid-cols-7 grid-rows-5 space-y-0.5">
+          {Array.from({ length: 5 }).map((week, weekIndex) => (
+            <div
+              key={weekIndex}
+              className="col-span-7 row-span-1 grid grid-cols-7"
+            >
+              {Array.from({ length: 7 }).map((day, dayIndex) => {
+                const movingDate = new Date(mostRecentSunday);
+                movingDate.setDate(
+                  mostRecentSunday.getDate() + (weekIndex * 7 + dayIndex),
+                );
+                movingDate.setHours(0, 0, 0, 0);
+                const dayOfTheMonth = movingDate.getDate();
 
-              const lastBookableDay = new Date(currentDate);
-              lastBookableDay.setDate(currentDate.getDate() + 28);
-              lastBookableDay.setHours(0, 0, 0, 0);
+                const lastBookableDay = new Date(currentDate);
+                lastBookableDay.setDate(currentDate.getDate() + 28);
+                lastBookableDay.setHours(0, 0, 0, 0);
 
-              const isDayUnbookable =
-                movingDate < currentDate || movingDate > lastBookableDay;
+                const isDayUnbookable =
+                  movingDate < currentDate || movingDate > lastBookableDay;
 
-              const isSelectedBookDate =
-                movingDate.getTime() === bookServiceDate?.getTime();
+                const isSelectedBookDate =
+                  movingDate.getTime() === bookServiceDate?.getTime();
 
-              return (
-                <div
-                  key={dayIndex}
-                  className={`relative flex items-center rounded-[2px] leading-[.5rem] ${isDayUnbookable ? "" : "hover:bg-neutral-400 "}`}
-                >
-                  <button
-                    disabled={isDayUnbookable}
-                    onClick={() => {
-                      handleOnClick(movingDate);
-                    }}
-                    className={` flex h-6 w-full items-center justify-center font-aperçu ${isDayUnbookable ? "text-[.35rem] text-neutral-500" : "text-[.625rem]"} font-bold tabular-nums leading-[.5rem] small-caps `}
+                return (
+                  <div
+                    key={dayIndex}
+                    className={`relative flex items-center rounded-[2px] leading-[.5rem] ${isDayUnbookable ? "" : "hover:bg-neutral-400 "}`}
                   >
-                    {dayOfTheMonth}
-                  </button>
+                    <button
+                      disabled={isDayUnbookable}
+                      onClick={() => {
+                        handleOnClick(movingDate);
+                      }}
+                      className={` flex h-6 w-full items-center justify-center font-aperçu ${isDayUnbookable ? "text-[.35rem] text-neutral-500" : "text-[.625rem]"} font-bold tabular-nums leading-[.5rem] small-caps `}
+                    >
+                      {dayOfTheMonth}
+                    </button>
 
-                  {isSelectedBookDate && (
-                    <motion.div
-                      layoutId="selectedBookDate"
-                      className="absolute left-0 top-0 h-full w-full rounded-[2px] bg-neutral-400 bg-opacity-50"
-                    ></motion.div>
-                  )}
-                </div>
-              );
+                    {isSelectedBookDate && (
+                      <motion.div
+                        layoutId="selectedBookDate"
+                        className="absolute left-0 top-0 h-full w-full rounded-[2px] bg-neutral-400 bg-opacity-50"
+                      ></motion.div>
+                    )}
+                  </div>
+                );
+              })}
+            </div>
+          ))}
+        </div>
+        <div className="flex w-full ps-2">
+          <LabelWIthAnimatedSlidingText
+            label={"date:"}
+            text={(bookServiceDate
+              ? new Date(bookServiceDate)
+              : new Date()
+            ).toLocaleDateString("en-US", {
+              weekday: "long",
+              year: "numeric",
+              month: "long",
+              day: "numeric",
             })}
-          </div>
-        ))}
-      </div>
-      <div className="">
-        <BookServiceTimePicker />
-      </div>
-
+          />
+        </div>
+        <div className="">
+          <BookServiceTimePicker />
+        </div>
       </main>
-      <footer className="flex w-full ps-2">
-        <LabelWIthAnimatedSlidingText
-          label={"date:"}
-          text={(bookServiceDate
-            ? new Date(bookServiceDate)
-            : new Date()
-          ).toLocaleDateString("en-US", {
-            weekday: "long",
-            year: "numeric",
-            month: "long",
-            day: "numeric",
-          })}
-        />
-      </footer>
     </div>
   );
 };
--- src/frontend/sections/book-service-modal/sub-components/book-service-time-picker.tsx
+++ src/frontend/sections/book-service-modal/sub-components/book-service-time-picker.tsx
@@ -3,27 +3,7 @@ import React from "react";
 const BookServiceTimePicker = () => {
   return (
     <div className="flex w-full flex-col rounded">
-      <div className="grid-cols-17 justify-centre flex w-full grid-rows-1 items-center border-b-[0.7px] border-black px-2">
-        {Array.from({ length: 17 }).map((_, hourIndex) => (
-          <div
-            key={hourIndex}
-            className="col-span-1 row-span-1 flex w-[calc(100%/17)] items-center justify-center bg-purple-400 text-[.5rem] font-bold"
-          >
-            {hourIndex % 2 !== 1 ? String(hourIndex + 7).padStart(2, "0") : ""}
-          </div>
-        ))}
-      </div>
 
-      <div className="h-6 relative w-full bg-green-400 px-2">
-        <div
-          className="w-10 h-6 rounded-[2px] bg-purple-400"
-          style={{
-            position: "absolute",
-            top: 0,
-            left: "0px",
-          }}
-        >123</div>
-      </div>
     </div>
   );
 };
