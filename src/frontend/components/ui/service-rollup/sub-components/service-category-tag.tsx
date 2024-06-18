af1fda9860ff1c472935a1e4e10c6bd164277927
Nuno Rodrigues <123973723+NVR-2023@users.noreply.github.com>
2024-06-18 11:56:57 +0100

    chore: move servieCOlortagMAp back to soruce file, because otehrwise Taiwlind doesn't compute classes

--- src/frontend/components/ui/service-rollup/sub-components/service-category-tag.tsx
+++ src/frontend/components/ui/service-rollup/sub-components/service-category-tag.tsx
@@ -1,5 +1,4 @@
 import InvertedTag from "../../styled-text-components/inverted-tag";
-import { serviceCAtegoryColorMap } from "@/utils/data/service-category-color-map";
 
 type ServiceCategoryRollupLabelProps = {
   category: string;
@@ -8,12 +7,33 @@ type ServiceCategoryRollupLabelProps = {
 const ServiceCategoryRollupTag = ({
   category,
 }: ServiceCategoryRollupLabelProps) => {
+  
+  const serviceCategoryColorMap = new Map([
+    ["cleaning", "bg-[#32CD32]"],
+    ["wardrobe", "bg-[#FF0000]"],
+    ["plumbing", "bg-[#AFEEEE]"],
+    ["electrical", "bg-[#FFFF00]"],
+    ["hvac", "bg-[#A020F0]"],
+    ["security", "bg-[#9400D3]"],
+    ["handyman", "bg-[#EE4B2B]"],
+    ["patching", "bg-[#DFFF00]"],
+    ["gardening", "bg-[#008000]"],
+    ["extermination", "bg-[#008080]"],
+    ["eventing", "bg-[#A020F0]"],
+    ["companionship", "bg-[#FF69B5]"],
+    ["grooming", "bg-[#000080]"],
+    ["nursing", "bg-[#86efac]"],
+    ["nannying", "bg-[#00FFFF]"],
+    ["petcare", "bg-[#964B00]"],
+    ["wellness", "bg-[#FFC0CB]"],
+  ]);
 
+  const serviceCategoryTagColor = serviceCategoryColorMap.get(category);
 
   return (
     <div className="flex">
       <div
-        className={`h-4.5 w-1.5 -skew-x-12 ${serviceCAtegoryColorMap.get(category)}`}
+        className={`h-4.5 w-1.5 -skew-x-12 ${serviceCategoryTagColor}`}
       ></div>
       <div className="flex h-4 -skew-x-12 items-center bg-[#acacac]">
         <span className="flex skew-x-12 items-center px-3">
