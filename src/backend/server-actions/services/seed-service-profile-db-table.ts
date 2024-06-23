6d58e6e464f87ebd96bc6cc02751865f1163b80b
Nuno Rodrigues <123973723+NVR-2023@users.noreply.github.com>
2024-06-23 14:44:11 +0100

    chore:

--- src/backend/server-actions/services/seed-service-profile-db-table.ts
+++ src/backend/server-actions/services/seed-service-profile-db-table.ts
@@ -1,6 +1,6 @@
 import { db } from "../../database/drizzle/db";
 import { services, serviceProfiles } from "@/backend/database/schema/schema";
-import { insertServiceProfileInDBTable } from "./service-db-functions-and-queries";
+import { insertServiceProfileInDBTable } from "./insert-service-profile-in-db-table";
 
 export type NewServiceType = typeof services.$inferInsert;
 export type NewServiceProfileType = typeof serviceProfiles.$inferInsert;
