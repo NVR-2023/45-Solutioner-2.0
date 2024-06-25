994e3c12954714d8a13c9cefbca3ec74dce9d8e4
Nuno Rodrigues <123973723+NVR-2023@users.noreply.github.com>
2024-06-25 19:20:29 +0100

    fix: set is CalendarExpanded to true when clsoing modal to avoid transition when re-opeining

--- src/frontend/sections/book-service-modal/book-service-modal.tsx
+++ src/frontend/sections/book-service-modal/book-service-modal.tsx
@@ -96,6 +96,7 @@ const BookServiceModal = () => {
 
   const handleOnCancel = () => {
     setIsBookServiceModalOpen(false);
+    setIsCalendarExpanded(true);
   };
 
   const handleOnSubmit = () => {
