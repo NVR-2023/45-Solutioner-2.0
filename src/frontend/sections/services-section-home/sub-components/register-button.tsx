c7c7a3cb6b6e36b90646eaffe6ae34cd38be8b4e
Nuno Rodrigues <123973723+NVR-2023@users.noreply.github.com>
2024-07-01 23:03:19 +0100

    chore:

--- src/frontend/sections/services-section-home/sub-components/register-button.tsx
+++ src/frontend/sections/services-section-home/sub-components/register-button.tsx
@@ -2,9 +2,11 @@ import Link from "next/link";
 
 const RegisterButton = () => {
   return (
-    <button className="items-h-10 center flex w-24 justify-center rounded bg-[#ff7714] px-2 py-1 text-base font-semibold tracking-widest text-neutral-100 small-caps">
-      <Link href="/register">register</Link>
-    </button>
+    <Link href="/register">
+      <button className="items-h-10 center flex w-24 justify-center rounded bg-[#ff7714] px-2 py-1 text-base font-semibold tracking-widest text-neutral-100 small-caps">
+        register
+      </button>{" "}
+    </Link>
   );
 };
 
