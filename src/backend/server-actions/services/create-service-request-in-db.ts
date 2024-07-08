eee5909312c1ca1f666211973c0cd9b7e5716c38
Nuno Rodrigues <123973723+NVR-2023@users.noreply.github.com>
2024-07-08 13:43:21 +0100

    fix: get userID from validateReques()

--- src/backend/server-actions/services/create-service-request-in-db.ts
+++ src/backend/server-actions/services/create-service-request-in-db.ts
@@ -1,20 +1,30 @@
 "use server";
 
+import { NextResponse } from "next/server";
 import { db } from "@/backend/database/drizzle/db";
 import { serviceRequests } from "@/backend/database/schema/schema";
 import {
   RecurrenceType,
   BookServiceModalObjectType,
 } from "@/frontend/sections/book-service-modal/book-service-modal";
+import { validateRequest } from "@/backend/lucia-auth/validate-request";
 
 export const createServiceRequestInDb = async (
   newServiceRequest: BookServiceModalObjectType,
 ) => {
   try {
+    const { user, session } = await validateRequest();
+    const userId: string = user?.id!;
+    if (!session || !userId) {
+      return new NextResponse("Unauthorized request", { status: 403 });
+    }
+
+    // server validation omitted here
+
     const insertedServiceRequest = await db
       .insert(serviceRequests)
       .values({
-        userId: newServiceRequest.userId!,
+        userId: userId,
         serviceId: newServiceRequest.serviceId!,
         dateOfService: newServiceRequest.date!,
         timeOfService: newServiceRequest.time!,
