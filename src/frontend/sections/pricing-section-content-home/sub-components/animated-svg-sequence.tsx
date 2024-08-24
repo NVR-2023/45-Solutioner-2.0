5c8494dfbf37b8f021dab6a48fb467f8aaaad4d6
Nuno Rodrigues <123973723+NVR-2023@users.noreply.github.com>
2024-08-24 21:53:07 +0100

    chore:

--- src/frontend/sections/pricing-section-content-home/sub-components/animated-svg-sequence.tsx
+++ src/frontend/sections/pricing-section-content-home/sub-components/animated-svg-sequence.tsx
@@ -1,35 +1,16 @@
-import { useRef } from "react";
-import { motion, useInView } from "framer-motion";
 import { svgEuro, svgHeart, svgStar } from "./svg-paths";
 import SVGMorph from "@/frontend/components/ui/animated-components/svg-morph";
 
-const VARIANTS = {
-  initial: { opacity: 0 },
-  animate: {
-    opacity: 1,
-    transition: {
-      duration: 0.3,
-      delay: 1,
-    },
-  },
-};
 const AnimatedSVGSequence = () => {
-  const ref = useRef(null);
-  const isInView = useInView(ref, { amount: 0.2 });
   return (
-    <motion.div
-      ref={ref}
-      variants={VARIANTS}
-      initial="initial"
-      animate={isInView ? "animate" : "initial"}
-    >
+    <div>
       <svg viewBox="0 0 192 192">
         <SVGMorph
           pathsArray={[svgEuro, svgStar, svgHeart, svgEuro]}
           duration={1}
         />
       </svg>
-    </motion.div>
+    </div>
   );
 };
 
--- src/frontend/sections/pricing-section-content-home/sub-components/pricing-animated-compoennt.tsx
+++ src/frontend/sections/pricing-section-content-home/sub-components/pricing-animated-compoennt.tsx
@@ -1,10 +1,33 @@
+import { useRef } from "react";
 import AnimatedSVGSequence from "./animated-svg-sequence";
 import TextInplaceInfiniteLoop from "@/frontend/components/ui/animated-components/text-inplace-infinite-loop";
 import HomepageSectionCaption from "@/frontend/components/ui/styled-text-components/homepage-section-caption";
 import AnimatedDiscountLoop from "./animated-discount-loop";
+import { motion, useInView } from "framer-motion";
+
+const VARIANTS = {
+  initial: { opacity: 0 },
+  animate: {
+    opacity: 1,
+    transition: {
+      duration: 0.3,
+      delay: 1,
+    },
+  },
+};
+
 const PricingAnimatedComponent = () => {
+  const ref = useRef(null);
+  const isInView = useInView(ref, { amount: 0.2 });
+
   return (
-    <div className="-mt-8 mb-2 flex w-48 justify-between text-[#fc6900]">
+    <motion.div
+      variants={VARIANTS}
+      initial="initial"
+      animate={isInView ? "animate" : "initial"}
+      ref={ref}
+      className="-mt-8 mb-2 flex w-48 justify-between text-[#fc6900]"
+    >
       <div className="flex size-20 flex-col">
         <AnimatedSVGSequence />
         <div className="flex justify-center">
@@ -18,7 +41,7 @@ const PricingAnimatedComponent = () => {
       <div className="mt-1,5">
         <AnimatedDiscountLoop />
       </div>
-    </div>
+    </motion.div>
   );
 };
 
