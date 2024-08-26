e28cad75dc8f675905d89557284599a8a7ea85b8
Nuno Rodrigues <123973723+NVR-2023@users.noreply.github.com>
2024-08-26 09:03:47 +0100

    chore:

--- src/frontend/sections/pricing-section-content-home/sub-components/animated-discount-loop.tsx
+++ src/frontend/sections/pricing-section-content-home/sub-components/animated-discount-loop.tsx
@@ -1,15 +1,42 @@
+import { motion, useInView } from "framer-motion";
+import { useRef } from "react";
+
+const VARIANTS = {
+  initial: { x: -25, opacity: 0 },
+  animate: {
+    x: 0,
+    opacity: 1,
+    transition: {
+      duration: 0.18,
+      delay: 1,
+      easing: [0.7, 0, 0.84, 0],
+    },
+  },
+};
+
 const AnimatedDiscountLoop = () => {
+  const ref = useRef(null);
+  const isInView = useInView(ref, { once: true, amount: 0.2 });
+
   return (
-    <div className="borer-[1px] flex flex-col items-center rounded border-[1pt] border-[#fc6900] px-2 py-1 ">
+    <div
+      ref={ref}
+      className="borer-[1px] flex flex-col items-center overflow-clip rounded border-[1pt] border-[#fc6900] px-2 py-1"
+    >
       <div className="flex w-full items-center justify-center border-b-[0.75pt] border-[#fc6900] text-sm font-bold small-caps">
         up to
       </div>
-      <div className="flex items-center py-1 text-4xl font-black">
+      <motion.div
+        variants={VARIANTS}
+        initial="initial"
+        animate={isInView ? "animate" : "initial"} // Trigger animation based on isInView
+        className="flex translate-y-0.5 items-center py-1 text-4xl font-black"
+      >
         <span className="">30</span>
         <span className="text-2xl">%</span>
-      </div>
+      </motion.div>
 
-      <div className="items-centre flex w-full justify-center border-t-[0.75pt] border-[#fc6900] text-sm font-bold small-caps">
+      <div className="flex w-full items-center justify-center border-t-[0.75pt] border-[#fc6900] text-sm font-bold small-caps">
         off
       </div>
     </div>
