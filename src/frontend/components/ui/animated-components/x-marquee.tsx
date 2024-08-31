ad664a68c8668d2f7b360b2656be179b31e23b06
Nuno Rodrigues <123973723+NVR-2023@users.noreply.github.com>
2024-08-31 22:23:59 +0100

    chore:

--- src/frontend/components/ui/animated-components/x-marquee.tsx
+++ src/frontend/components/ui/animated-components/x-marquee.tsx
@@ -25,12 +25,12 @@ const XMarquee = () => {
           }
 
           .animate-marquee {
-            animation: marquee 25s linear infinite;
+            animation: marquee 10s linear infinite;
             animation-direction: reverse;
           }
 
           .animate-marquee2 {
-            animation: marquee2 25s linear infinite;
+            animation: marquee2 10s linear infinite;
             animation-direction: reverse;
 
           }
@@ -38,20 +38,20 @@ const XMarquee = () => {
       </style>
 
       <div className="relative flex overflow-x-hidden">
-        <div className="animate-marquee whitespace-nowrap py-12">
-          <span className="mx-4 text-4xl">Marquee Item 1</span>
-          <span className="mx-4 text-4xl">Marquee Item 2</span>
-          <span className="mx-4 text-4xl">Marquee Item 3</span>
-          <span className="mx-4 text-4xl">Marquee Item 4</span>
-          <span className="mx-4 text-4xl">Marquee Item 5</span>
+        <div className="animate-marquee whitespace-nowrap">
+          <span className="mx-1 text-xs">Item 1</span>
+          <span className="mx-1 text-xs">Item 2</span>
+          <span className="mx-1 text-xs">Item 3</span>
+          <span className="mx-1 text-xs">Item 4</span>
+          <span className="mx-1 text-xs">Item 5</span>
         </div>
 
-        <div className="animate-marquee2 absolute top-0 whitespace-nowrap py-12">
-          <span className="mx-4 text-4xl">Marquee Item 1</span>
-          <span className="mx-4 text-4xl">Marquee Item 2</span>
-          <span className="mx-4 text-4xl">Marquee Item 3</span>
-          <span className="mx-4 text-4xl">Marquee Item 4</span>
-          <span className="mx-4 text-4xl">Marquee Item 5</span>
+        <div className="animate-marquee2 absolute top-0 whitespace-nowrap">
+          <span className="mx-1 text-xs">Item 1</span>
+          <span className="mx-1 text-xs">Item 2</span>
+          <span className="mx-1 text-xs">Item 3</span>
+          <span className="mx-1 text-xs">Item 4</span>
+          <span className="mx-1 text-xs">Item 5</span>
         </div>
       </div>
     </>
